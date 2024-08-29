1c56522978cfaee5989f6897b164cd2ffc86c600
Swetha-Pradeep <swetha.pradeep@philips.com>
2024-08-29 17:07:44 +0530

    fixed segmentation fault

--- stats.cpp
+++ stats.cpp
@@ -5,17 +5,26 @@ Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& numVec
     float avg = 0.0;
     float max_val = 0.0;
     float min_val = 0.0;
-    for (auto i = numVec.begin(); i != numVec.end(); i++) {
-        avg = avg + *i;
-    }
+    Statistics::Stats stats;
+    if (!numVec.empty()){
+        for (auto i = numVec.begin(); i != numVec.end(); i++) {
+            avg = avg + *i;
+        }
     avg = avg / numVec.size();
 
-    max_val= *max_element(numVec.begin(), numVec.end());
+    max_val = *max_element(numVec.begin(), numVec.end());
     min_val = *min_element(numVec.begin(), numVec.end());
 
-    Statistics::Stats stats;
+    
     stats.average = avg;
     stats.max = max_val;
     stats.min = min_val;
+    }
+    else {
+        stats.average = NAN ;
+        stats.max = NAN;
+        stats.min = NAN;
+    }
+
     return stats;
 }
