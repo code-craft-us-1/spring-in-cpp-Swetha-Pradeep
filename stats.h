82cad19f070a49944123a2e317638bfe6c6ccc27
Swetha-Pradeep <swetha.pradeep@philips.com>
2024-08-29 15:05:37 +0530

    fixed the stats not a type error

--- stats.h
+++ stats.h
@@ -2,10 +2,10 @@
 
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
-    struct{
+    struct Stats {
         float average;
         float max;
         float min;
-    }Stats;
+    };
     Stats ComputeStatistics(const std::vector<float>& );
 }
