902810d76b815b0c9d358ec49cf5e641d607a4e6
Swetha-Pradeep <swetha.pradeep@philips.com>
2024-08-29 14:50:19 +0530

    first assignment

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,21 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& numVec) {
     //Implement statistics here
+    float avg = 0.0;
+    float max_val = 0.0;
+    float min_val = 0.0;
+    for (int i = numVec.begin(); i != numVec.end(); i++) {
+        avg = avg + *i;
+    }
+    average = avg / numVec.size();
+
+    max_val= *max_element(numVec.begin(), numVec.end());
+    min_val = *min_element(numVec.begin(), numVec.end());
+
+    Stats stats;
+    stats.average = avg;
+    stats.max = max_val;
+    stats.min = min_val;
+    return stats;
 }
